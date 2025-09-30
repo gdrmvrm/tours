74f9b8352a3e0dea7e9056bfda6b6dd433a33cbe
gdrmvrm <gdrmvrm@gmail.com>
2025-09-30 13:28:33 +0100

    Fetch tours data from API on component mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
+  const [list, setList] = useState([]);
+
+  const fetchTours = async () => {
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json();
+      console.log(data);
+      setList(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
