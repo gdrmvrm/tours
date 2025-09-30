bef5dbefebf8a84cadc5ec4313af00a167350be9
gdrmvrm <gdrmvrm@gmail.com>
2025-09-30 14:47:57 +0100

    Read More toggle functionality

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const onReadMore = () => {
+    setReadMore(!readMore);
+  };
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button type="button" className="info-btn" onClick={onReadMore}>
+            {readMore ? 'Show Less' : 'Read More'}
+          </button>
+        </p>
         <button type="button" className="btn btn-block delete-btn" onClick={() => removeTour(id)}>
           Not Interested
         </button>
